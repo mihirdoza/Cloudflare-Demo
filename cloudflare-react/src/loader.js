a7410038c9395eb41c1460ddbb463cc5514b4374
MohitDharveeInfotech <mohit.r@dharveeinfotech.com>
2025-01-10 13:09:26 +0530

    first

---
+++ cloudflare-react/src/loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="loaderdiv">
+      <div className="loader" aria-hidden="true"></div>
+    </div>
+  )
+}
+
+export default Loader
